b1d665626d7d9d5dcd207f1b5d6b44e7d5748e1f
kar <juancarlosalgoritmo@gmail.com>
2022-08-09 21:25:33 -0500

    arquitecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// routes
+import {LoginComponent} from './views/login/login.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component:LoginComponent, path:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// material
+import {MatButtonModule} from '@angular/material/button';
+import { NavHeaderComponent } from './components/nav-header/nav-header.component';
+import { LoginComponent } from './views/login/login.component';
+import { TemplatesComponent } from './views/templates/templates.component';
+import { CardTemplateComponent } from './views/templates/components/card-template/card-template.component';
+import { TestsComponent } from './views/tests/tests.component';
+import { CardTestComponent } from './views/tests/components/card-test/card-test.component';
+import { ResultsComponent } from './views/results/results.component';
+import { UsersComponent } from './views/users/users.component';
+import { CardUserComponent } from './views/users/components/card-user/card-user.component';
+import { ReportsComponent } from './views/reports/reports.component';
+import { SettingsComponent } from './views/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavHeaderComponent,
+    LoginComponent,
+    TemplatesComponent,
+    CardTemplateComponent,
+    TestsComponent,
+    CardTestComponent,
+    ResultsComponent,
+    UsersComponent,
+    CardUserComponent,
+    ReportsComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
