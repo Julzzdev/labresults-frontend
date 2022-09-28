b395780381eead0b80cbcb4c6efc43694e701f76
Julio Olvera <julioolveraunt@gmail.com>
2022-09-28 14:28:38 -0500

    test to deploy heroku env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  host: 'http://localhost:3000/',
+  host: 'https://labresults-api.herokuapp.com/',
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  host: 'http://localhost:3000/',
+  host: 'https://labresults-api.herokuapp.com/',
 }
 /*
  * For easier debugging in development mode, you can import the following file
